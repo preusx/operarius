636e02eb1dde4d3d3be6f7d4ed6ce003690b73df
Alex Tkachenko <preusx.dev@gmail.com>
2019-11-30 22:16:17 +0200

    Added separate eslint cli plugin

---
+++ packages/vue-cli-plugin-eslint/generator.js
@@ -0,0 +1,20 @@
+const { version } = require('./package.json');
+const ADDED_ESLINT_CONFIG = 'eslint:recommended';
+const ESLINT_CONFIG = '@operarius/vue-eslint-config';
+
+module.exports = api => {
+  const { eslintConfig } = api.generator.pkg;
+
+  const index = eslintConfig.extends.indexOf(ADDED_ESLINT_CONFIG);
+  if (index !== -1) {
+    eslintConfig.extends.splice(index, 1);
+  }
+
+  eslintConfig.extends.push(ESLINT_CONFIG);
+  api.extendPackage({
+    eslintConfig,
+    devDependencies: {
+      [ESLINT_CONFIG]: `^${version}`,
+    },
+  });
+}
